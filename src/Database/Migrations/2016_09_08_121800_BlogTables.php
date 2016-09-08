744be41024b8b33bf38b721e158eafe1e122c32f
Mike Fleming <hosting@magnetion.com>
2016-09-08 15:28:19 -0400

    Base migrations

--- src/Database/Migrations/2016_09_08_121800_BlogTables.php
+++ src/Database/Migrations/2016_09_08_121800_BlogTables.php
@@ -13,7 +13,57 @@ class BlogTables extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('blog_post', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('author_id');
+            $table->integer('publish_date');
+            $table->string('slug', 255);
+            $table->string('title', 255);
+            $table->text('content')->nullable();
+            $table->text('summary')->nullable();
+            $table->integer('status');
+            $table->integer('comments_enabled')->default(0);
+            $table->integer('comments_count')->default(0);
+            $table->string('post_type', 20);
+            $table->string('external_id', 100)->nullable();
+            $table->timestamps();
+        });
+
+
+        Schema::create('blog_comment', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('post_id');
+            $table->string('display_name', 75);
+            $table->string('email', 75)->nullable();
+            $table->string('ip_address', 25)->nullable();
+            $table->text('content');
+            $table->integer('approved')->default(0);
+            $table->integer('parent_id')->nullable();
+            $table->string('external_id', 100)->nullable();
+            $table->timestamps();
+        });
+
+
+        Schema::create('blog_metadata', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('slug', 255);
+            $table->string('type', 50);
+            $table->string('external_id', 100)->nullable();
+            $table->timestamps();
+        });
+
+
+        Schema::create('blog_taxonomy', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('post_id');
+            $table->integer('metadata_id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +73,9 @@ class BlogTables extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('blog_post');
+        Schema::drop('blog_comment');
+        Schema::drop('blog_metadata');
+        Schema::drop('blog_taxonomy');
     }
 }
