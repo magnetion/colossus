f94f82d0a45e3ac166c7d212e44c8ff6715c673a
Mike Fleming <hosting@magnetion.com>
2016-09-22 14:18:57 -0400

    Homepage post model update

--- src/Controllers/FrontController.php
+++ src/Controllers/FrontController.php
@@ -15,7 +15,7 @@ class FrontController extends \Illuminate\Routing\Controller
 
     public function buildHomepage()
     {
-        $posts = \Magnetion\Colossus\Models\Post::with('author', 'categories')->where('status', 1)->orderBy('publish_date', 'desc')->paginate(10);
+        $posts = \Magnetion\Colossus\Models\Post::with('author', 'categories')->where('status', 1)->where('post_type', 'post')->orderBy('publish_date', 'desc')->paginate(10);
 
         return View::make('home', compact('posts'));
     }
